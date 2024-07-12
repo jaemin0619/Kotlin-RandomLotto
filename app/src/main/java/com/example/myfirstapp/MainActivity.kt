53f168c9ce5fd2099615db708fbcc3dfd66870b5
jaemin0619 <wjdwoals000619@naver.com>
2024-07-12 21:47:32 +0900

    Second

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,3 +1,10 @@
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myfirstapp.R
+import kotlin.random.Random
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
